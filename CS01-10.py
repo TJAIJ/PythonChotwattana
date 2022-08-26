e9553a27d228605dcc934ab756c620ab299f3822
TJAIJ <38485@prc.ac.th>
2022-08-26 16:23:02 +0700

    8/26/2022
    ---

---
+++ CS01-10.py
@@ -0,0 +1,3 @@
+num = int(input("Input Number: "))
+for i in range (100):
+    print("Result :",i+num)
---
+++ CS01-12.py
@@ -0,0 +1,11 @@
+Animal = ["Cat","Dog","Bird","Cat","Bird","Dog"]
+for x in Animal:
+    if x == "Cat":
+        print(x)
+
+print("")
+
+for x in range(10) :
+    print(x)
+    for y in range(10) :
+        print(y)
